e93fd7df10eafc51f647de772fe109020949455c
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2021-05-23 10:38:59 -0500

    ByteCoin Done

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Juan Ramírez Blancas on 23/05/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
---
+++ ByteCoin/ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Juan Ramírez Blancas on 23/05/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel {
+    let rate: Double
+}
