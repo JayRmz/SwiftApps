57e8dc2ff97064f78bbdbc3d5c0f9853ef5346ba
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2021-05-30 20:47:56 -0500

    Flash chat done

---
+++ FlashChat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Juan Ramírez Blancas on 30/05/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //email Address
+    let body: String //Message body
+}
